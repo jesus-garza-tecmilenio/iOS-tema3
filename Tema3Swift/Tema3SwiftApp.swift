787ec0d686e5fb8c55038e78781c27d1fa6cbfb8
jesusgarza <jesus.garza.gro@hotmail.com>
2025-08-26 16:06:14 -0600

    Initial Commit

---
+++ Tema3Swift/Tema3SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tema3SwiftApp.swift
+//  Tema3Swift
+//
+//  Created by JESUS GARZA on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Tema3SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
